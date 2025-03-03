a90ef3e752f68a2e0cbce0f940618874a9b45c4e
Tidaleus <Tidaleus@DESKTOP-F0V0C0L>
2025-03-03 03:12:59 -0700

    Add project files.

---
+++ More Revive HP/Patches/PlayerAvatarPatch.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using More_Revive_HP;
+
+namespace More_Revive_HP.Patches
+{
+    [HarmonyPatch(typeof(PlayerAvatar), "ReviveRPC")]
+    class PlayerAvatarPatch
+    {
+        static void Postfix(PlayerAvatar __instance, bool _revivedByTruck)
+        {
+
+            int ExtraHealth = Plugin.GetExtraHealth();
+
+            if (__instance.photonView.IsMine)
+            {
+                __instance.playerHealth.HealOther(ExtraHealth, true); 
+            }
+        }
+    }
+}
---
+++ More Revive HP/Plugin.cs
@@ -0,0 +1,44 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+using More_Revive_HP.Patches;
+using BepInEx.Configuration;
+
+[BepInPlugin(modGUID, modName, modVersion)]
+public class Plugin : BaseUnityPlugin
+{
+
+    public const string modGUID = "Tidaleus.MoreReviveHP";
+    public const string modName = "More Revive HP";
+    public const string modVersion = "1.0.0";
+
+    private readonly Harmony harmony = new Harmony(modGUID);
+
+    private static Plugin Instance;
+
+    // Configuration fields
+    private ConfigEntry<int> ExtraHealth;
+
+
+    void Awake()
+    {
+
+        Logger.LogInfo($"{modGUID} has loaded");
+
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+
+        ExtraHealth = Config.Bind("General", "DuplicationAmount", 19, "Extra Health to add to the 1 you spawn with, therefore make this something 1 through 99.");
+
+
+        harmony.PatchAll(typeof(Plugin));
+        harmony.PatchAll(typeof(PlayerAvatarPatch));
+    }
+
+    public static int GetExtraHealth() => Instance.ExtraHealth.Value;
+
+}
+
